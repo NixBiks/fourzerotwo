83bb5c3bf150d573e1aebda17bcbf7c177016cba
Nicolai Bjerre Pedersen <mr.bjerre@gmail.com>
2020-10-24 17:30:54 +0200

    Added very basic login

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import { Button } from '@material-ui/core';
+
+firebase.initializeApp(// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  {
+    apiKey: "AIzaSyBIVo6pQ2LQJzVJ3nKmiWfOXk9HMb5-iic",
+    authDomain: "fourzerotwo-402.firebaseapp.com",
+    databaseURL: "https://fourzerotwo-402.firebaseio.com",
+    projectId: "fourzerotwo-402",
+    storageBucket: "fourzerotwo-402.appspot.com",
+    messagingSenderId: "219373099131",
+    appId: "1:219373099131:web:02469148b073cb2b436ade",
+    measurementId: "G-JKXN7R0NNE"
+  })
+const provider = new firebase.auth.GoogleAuthProvider();
+
 
 function App() {
+
+  const signIn = () => {
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      console.log(result)
+
+      if (result.credential) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential;
+        // The signed-in user info.
+        var user = result.user;
+        // ...
+
+      }
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button onClick={signIn}>Log in</Button>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/hooks/useAuth.ts
@@ -0,0 +1,6 @@
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import * as firebase from "firebase/app";
+
+import "firebase/auth";
+
+const provider = new firebase.auth.GoogleAuthProvider();
